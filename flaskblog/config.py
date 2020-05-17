ef4ad864a78a424257d858df3452e52cffe17a36
fruparel25 <francyruparel25@gmail.com>
2020-05-17 11:34:10 +0530

    Your_commit_message_here

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+	SECRET_KEY='6ab0bdaa8a2c8ae0175a85d1b617ad21'
+	SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+	MAIL_SERVER='smtp.googlemail.com'
+	MAIL_PORT=587
+	MAIL_USE_TLS=True
+	MAIL_USERNAME="francyruparel25@gmail.com"
+	MAIL_PASSWORD="Vidhyanshi25"
